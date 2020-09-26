48c880464cefca5d830defa2dda2df0e15d883e7
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-09-25 21:13:11 -0300

    first commit

---
+++ source/hello_world/main.py
@@ -0,0 +1,14 @@
+#conding: utf-8
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+def build():
+    return Label(text = "Hello World")
+
+hello_world = App()
+hello_world.build = build
+hello_world.run()
+
+'''executar o arquivo main.py no terminal dentro do ambiente virtual'''
+

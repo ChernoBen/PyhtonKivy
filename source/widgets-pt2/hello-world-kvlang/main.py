93aa9a5ddbd5e1cee0543722e59d4fbdd053d5cb
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-09-27 19:39:26 -0300

    Primeiros passos com kvLang

---
+++ source/widgets-pt2/hello-world-kvlang/main.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+from kivy.app import App
+
+'''a classe deve ter o mesmo nome do arquivo kv em minusculo para poder importar o layout'''
+class hello(App):
+    pass
+
+
+
+hello().run()

3023d94ca3fce5995a6a202a6719d1eb9fb41e80
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-10 20:17:10 -0300

    testes de otimização

--- source/002_imprimindo_textos/main.py
+++ source/002_imprimindo_textos/main.py
@@ -3,6 +3,35 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
+N = 32  # dimensao do tabuleiro
+linha = [False] * N
+diagAsc = [False] * (2 * N - 1)
+diagDesc = [False] * (2 * N - 1)
+numSol = 0
+solucao = [0] * N
+
+
+def rainha(col):
+    gabarito = []
+    if col < N:
+        for lin in range(N):
+            if linha[lin] or diagAsc[lin + col] or diagDesc[lin - col + N - 1]:
+                continue
+            solucao[col] = lin
+            linha[lin] = diagAsc[lin + col] = diagDesc[lin - col + N - 1] = True
+            rainha(col + 1)
+            linha[lin] = diagAsc[lin + col] = diagDesc[lin - col + N - 1] = False
+    else:
+        gabarito.append(solucao)
+        print(solucao)
+        global numSol
+        numSol = numSol + 1
+        if len(gabarito) == numSol - 1:
+            return gabarito
+
+
+sol = rainha(0)
+print("Num.Solucoes= ", numSol)
 
 def build():
     lb = Label()
